3121a85a18191093c801c8d92482aa11bd41dabc
Ata-92 <atakal92@gmail.com>
2021-07-07 22:30:30 +0300

    Import model to admin and add some field options

--- src/fscohort/models.py
+++ src/fscohort/models.py
@@ -8,4 +8,7 @@ def validate_number(value):
 class Student(models.Model):
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=40)
-    number = models.IntegerField(validators=[validate_number])
+    number = models.IntegerField(validators=[validate_number], blank=True, null=True)
+
+    def __str__(self):
+        return self.first_name
