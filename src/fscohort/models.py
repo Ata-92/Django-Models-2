2a67b43f2115f00461611ce74574a0c0e2048be4
Ata-92 <atakal92@gmail.com>
2021-07-07 22:13:15 +0300

    Create first model

--- src/fscohort/models.py
+++ src/fscohort/models.py
@@ -1,3 +1,11 @@
 from django.db import models
-
+from django.core.exceptions import ValidationError
 # Create your models here.
+
+def validate_number(value):
+    if value > 5000:
+        raise ValidationError("Please enter a valid number less than 5000")
+class Student(models.Model):
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=40)
+    number = models.IntegerField(validators=[validate_number])
