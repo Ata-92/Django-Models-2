a432de1704c927c6f7c6161fb5583c483495d52f
Ata-92 <atakal92@gmail.com>
2021-07-07 23:44:54 +0300

    Add another model with one-to-many relationship

--- src/dj_relationships/admin.py
+++ src/dj_relationships/admin.py
@@ -3,4 +3,5 @@ from django.contrib import admin
 from dj_relationships.models import Creator, Language
 
 # Register your models here.
-admin.site.register(Creator, Language)
+admin.site.register(Creator)
+admin.site.register(Language)
